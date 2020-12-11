8966ef2dc72a314a1a12b8dc7cfcbb04ce26a517
jake <www.1240035451@qq.com>
2020-12-11 16:23:31 +0800

    翻译 URL 标示

--- routes/web.php
+++ routes/web.php
@@ -48,7 +48,8 @@ Route::resource('users', 'UsersController', ['only' => ['show', 'update', 'edit'
 
 
 // 话题路由
-Route::resource('topics', 'TopicsController', ['only' => ['index', 'show', 'create', 'store', 'update', 'edit', 'destroy']]);
+Route::resource('topics', 'TopicsController', ['only' => ['index', 'create', 'store', 'update', 'edit', 'destroy']]);
+Route::get('topics/{topic}/{slug?}', 'TopicsController@show')->name('topics.show');
 
 // 话题分类路由
 Route::resource('categories', 'categoriesController', ['only' => ['show']]);
